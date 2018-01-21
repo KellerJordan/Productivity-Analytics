40af198efd02617c66abf13255abdb6178746d66
reevesrs24 <reevesrs24@gmail.com>
2018-01-20 19:49:04 -0800

    Added CORS bypass for Chrome

--- insyte_chrome/insyte.js
+++ insyte_chrome/insyte.js
@@ -1,20 +1,36 @@
-chrome.tabs.onCreated.addListener((tab) => {
-    if (tab.url) {
-        console.log(`Opened tab ${tab.id} with URL ${tab.url}.`);
-    }
-});
 
-chrome.tabs.onRemoved.addListener((tabId) => {
-    console.log(`Closed tab ${tabId}`);
-});
 
-chrome.tabs.onUpdated.addListener((tabId, changes) => {
-    if (changes.url) {
-        console.log(`URL changed to ${changes.url} in tab ${tabId}.`);
+
+function getAllURLS() {
+	console.log("Getting all URLs..");
+	
+	var scripts = document.getElementsByTagName("script");
+
+	for (var i = 0; i < scripts.length; i++) {
+     if (scripts[i].src) console.log(i,scripts[i].src)
+  }
+	blockURL("hello");
+	   
+}
+
+function blockURL(url) {
+	console.log("Blocking.. " + url);
+	var xhttp = new XMLHttpRequest();
+  	xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
     }
-});
+  };
+
+  xhttp.open("GET", "https://httpbin.org/get", true); /* Test endpoint */
+  xhttp.send();
+}
 
 window.addEventListener("load", function(){
-    // ....
-    console.log("TEST");
+
+    console.log("On Load..");
+    getAllURLS();
 });
+
+
+
