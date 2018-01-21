a7159f99800cbab3c711c096bcca2efd4c76abb5
reevesrs24 <reevesrs24@gmail.com>
2018-01-20 21:57:48 -0800

    Added json array to post

--- insyte_chrome/insyte.js
+++ insyte_chrome/insyte.js
@@ -9,12 +9,15 @@ function getAllURLS() {
 	for (var i = 0; i < scripts.length; i++) {
      if (scripts[i].src) console.log(i,scripts[i].src)
   }
-	blockURL("hello");
+	blockURL(scripts);
 	   
 }
 
-function blockURL(url) {
-	console.log("Blocking.. " + url);
+function blockURL(urls) {
+	console.log("Blocking.. " );
+
+	var myJson = JSON.stringify(urls);
+
 	var xhttp = new XMLHttpRequest();
   	xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -22,7 +25,7 @@ function blockURL(url) {
     }
   };
 
-  xhttp.open("POST", "http://127.0.0.1:8000/inspect/", true); /* Test endpoint */
+  xhttp.open("POST", "http://127.0.0.1:8000/inspect/", myJson, true); /* Test endpoint */
   xhttp.send();
 }
 
