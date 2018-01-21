f7299895cd85032376ce8e78e5dfe782fbc98e55
reevesrs24 <reevesrs24@gmail.com>
2018-01-20 22:36:02 -0800

    Fixed bug in url post

--- insyte_chrome/insyte.js
+++ insyte_chrome/insyte.js
@@ -4,17 +4,26 @@
 function getAllURLS() {
 	console.log("Getting all URLs..");
 	
+	var urls = [];
 	var scripts = document.getElementsByTagName("script");
+	var links = document.links;
+	var img_src = document.getElementsByTagName("img");
 
-	for (var i = 0; i < scripts.length; i++) {
-     if (scripts[i].src) console.log(i,scripts[i].src)
-  }
-	blockURL(scripts);
+	for (var i = 0; i < scripts.length; i++)
+	 	if (scripts[i].src) urls.push(scripts[i].src);
+   
+    for (var i = 0; i < links.length; i++)
+   		if (links[i].href) urls.push(links[i].href);
+
+   	for (var i = 0; i < img_src.length; i++)
+   		if (img_src[i].src) urls.push(img_src[i].src);
+
+	checkURLS(urls);
 	   
 }
 
-function blockURL(urls) {
-	console.log("Blocking.. " );
+function checkURLS(urls) {
+	console.log("Checking.. " );
 
 	var myJson = JSON.stringify(urls);
 
@@ -22,12 +31,19 @@ function blockURL(urls) {
   	xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       console.log(this.responseText);
+      if (this.responseText != null)
+      	blockURLS(this.responseText);
     }
   };
 
   xhttp.open("POST", "http://127.0.0.1:8000/inspect/", myJson, true); /* Test endpoint */
   xhttp.send();
 }
+
+function blockURLS(urls) {
+	console.log("Blocking..");
+}
+
 
 window.addEventListener("load", function(){
 
